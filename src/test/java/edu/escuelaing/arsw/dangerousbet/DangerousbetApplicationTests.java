feece174fcfa8c73a915bda8f051e27cef5c8947
jsr25 <ramosjuans@live.com.ar>
2021-03-18 08:03:10 -0500

    Correccion error

--- src/test/java/edu/escuelaing/arsw/dangerousbet/DangerousbetApplicationTests.java
+++ src/test/java/edu/escuelaing/arsw/dangerousbet/DangerousbetApplicationTests.java
@@ -3,7 +3,7 @@ package edu.escuelaing.arsw.dangerousbet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DangerousbetApplicationTests {
 
 	@Test
